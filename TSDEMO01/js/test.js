a1c9cd87518e61d0ffb52288878e4c92a734ce70
luoquan1982 <luo.quan1982@gmail.com>
2020-03-15 20:53:35 +0800

    初始化项目

---
+++ TSDEMO01/js/test.js
@@ -0,0 +1,10 @@
+"use strict";
+/*
+    vscode配置自动编译
+    
+    1.第一步 tsc --init 生成tsconfig.json  去掉 outDir 配置的注释
+    
+    2.第二步 vscode 菜单栏 -> 终端 -> 运行任务 -> tsc:监视 - tsconfig.json
+*/
+console.log('Hello Typescript');
+var str = "你好ts";
