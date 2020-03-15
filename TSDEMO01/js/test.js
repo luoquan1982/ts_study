51c6ab5e694ae2c5f468aff9fd498e9c4958f76b
luoquan1982 <luo.quan1982@gmail.com>
2020-03-15 22:14:45 +0800

    枚举类型

--- TSDEMO01/js/test.js
+++ TSDEMO01/js/test.js
@@ -6,5 +6,18 @@
     
     2.第二步 vscode 菜单栏 -> 终端 -> 运行任务 -> tsc:监视 - tsconfig.json
 */
-console.log('Hello Typescript');
+console.log("Hello Typescript");
 var str = "你好ts";
+var arr1 = [1, 2, 3];
+var arr2 = [1, 2, 3];
+var arr3 = [123, "hello"];
+var arr4 = ["hello", 123];
+console.log(arr3);
+var Color;
+(function (Color) {
+    Color[Color["red"] = 0] = "red";
+    Color[Color["orange"] = 3] = "orange";
+    Color[Color["blue"] = 4] = "blue";
+})(Color || (Color = {}));
+var c = Color.blue;
+console.log(c);
--- TSDEMO01/test.ts
+++ TSDEMO01/test.ts
@@ -5,5 +5,22 @@
     
     2.第二步 vscode 菜单栏 -> 终端 -> 运行任务 -> tsc:监视 - tsconfig.json
 */
-console.log('Hello Typescript');
+console.log("Hello Typescript");
 const str: string = "你好ts";
+
+let arr1: number[] = [1, 2, 3];
+let arr2: Array<number> = [1, 2, 3];
+
+let arr3: [number, string] = [123, "hello"];
+let arr4: Array<number | string> = ["hello", 123];
+
+console.log(arr3);
+
+enum Color {
+  red,
+  orange = 3,
+  blue
+}
+
+let c: Color = Color.blue;
+console.log(c);
